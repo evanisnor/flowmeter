ee2d6549d8901ad42d321d8cf05f9253087fbc61
Evan Isnor <ewisnor@gmail.com>
2024-02-12 19:40:25 -0400

    base project

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+  namespace = "com.evanisnor.pomodoro"
+  compileSdk = 34
+
+  defaultConfig {
+    applicationId = "com.evanisnor.pomodoro"
+    minSdk = 29
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+
+    vectorDrawables {
+      useSupportLibrary = true
+    }
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_20
+    targetCompatibility = JavaVersion.VERSION_20
+  }
+  kotlinOptions {
+    jvmTarget = "20"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.5.9"
+  }
+  packaging {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
+    }
+  }
+}
+
+dependencies {
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.androidx.ui)
+  implementation(libs.androidx.ui.graphics)
+  implementation(libs.androidx.ui.tooling.preview)
+  implementation(libs.androidx.material3)
+  implementation(libs.circuit)
+
+  testImplementation(libs.junit)
+
+  debugImplementation(libs.androidx.ui.tooling)
+  debugImplementation(libs.androidx.ui.test.manifest)
+}
