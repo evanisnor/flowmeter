8bfa7e12c464dcd890ea14f42d6640f75dc6efd2
Evan Isnor <ewisnor@gmail.com>
2024-02-12 20:28:49 -0400

    circuit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
   implementation(libs.androidx.ui.graphics)
   implementation(libs.androidx.ui.tooling.preview)
   implementation(libs.androidx.material3)
-  implementation(libs.circuit)
+  implementation(libs.bundles.circuit)
 
   testImplementation(libs.junit)
 
