c82a2c0c87f5f48d11d49d68a5fb26356be2007c
Evan Isnor <ewisnor@gmail.com>
2024-02-19 17:46:08 -0400

    Remove vibration code

--- app/src/main/kotlin/com/evanisnor/flowmeter/features/flowtimesession/domain/AttentionGrabber.kt
+++ app/src/main/kotlin/com/evanisnor/flowmeter/features/flowtimesession/domain/AttentionGrabber.kt
@@ -1,8 +1,6 @@
 package com.evanisnor.flowmeter.features.flowtimesession.domain
 
 import android.media.RingtoneManager
-import android.os.Build
-import android.os.VibrationEffect
 import javax.inject.Inject
 
 /**
@@ -17,14 +15,6 @@ class AttentionGrabber @Inject constructor(
   }
 
   fun playBreakEndNoise() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-      VibrationEffect.startComposition()
-        .addPrimitive(VibrationEffect.Composition.PRIMITIVE_SLOW_RISE)
-        .addPrimitive(VibrationEffect.Composition.PRIMITIVE_CLICK)
-        .compose()
-    } else {
-      VibrationEffect.createOneShot(200L, VibrationEffect.DEFAULT_AMPLITUDE)
-    }
     ringtoneManager.getRingtone(RingtoneManager.TYPE_NOTIFICATION).play()
   }
 
