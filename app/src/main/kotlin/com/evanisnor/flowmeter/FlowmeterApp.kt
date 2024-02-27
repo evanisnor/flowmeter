7542810845fe1629cacecebcf477455af678be18
Evan Isnor <ewisnor@gmail.com>
2024-02-26 20:57:35 -0400

    move app component to its own file

--- app/src/main/kotlin/com/evanisnor/flowmeter/FlowmeterApp.kt
+++ app/src/main/kotlin/com/evanisnor/flowmeter/FlowmeterApp.kt
@@ -1,42 +1,14 @@
 package com.evanisnor.flowmeter
 
 import android.app.Application
-import android.content.Context
-import androidx.activity.ComponentActivity
 import androidx.work.Configuration
 import androidx.work.WorkManager
-import com.evanisnor.flowmeter.di.AnvilInjector
-import com.evanisnor.flowmeter.di.AppScope
-import com.evanisnor.flowmeter.di.SingleIn
-import com.evanisnor.flowmeter.system.NotificationSystem
-import com.evanisnor.flowmeter.system.WorkerFactoryFactory
-import com.squareup.anvil.annotations.MergeComponent
-import dagger.BindsInstance
-import dagger.Component
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import timber.log.Timber.DebugTree
 
-
-/**
- * Top-level component for [AppScope]
- */
-@SingleIn(AppScope::class)
-@MergeComponent(AppScope::class)
-interface FlowmeterAppComponent {
-  fun activityInjectors(): Map<Class<out ComponentActivity>, AnvilInjector<*>>
-  fun workerFactoryFactory(): WorkerFactoryFactory
-  fun notificationSystem(): NotificationSystem
-
-  @Component.Builder
-  interface Builder {
-    @BindsInstance
-    fun context(context: Context): Builder
-    fun build(): FlowmeterAppComponent
-  }
-}
 
 /**
  * App entry-point class.
---
+++ app/src/main/kotlin/com/evanisnor/flowmeter/FlowmeterAppComponent.kt
@@ -0,0 +1,30 @@
+package com.evanisnor.flowmeter
+
+import android.content.Context
+import androidx.activity.ComponentActivity
+import com.evanisnor.flowmeter.di.AnvilInjector
+import com.evanisnor.flowmeter.di.AppScope
+import com.evanisnor.flowmeter.di.SingleIn
+import com.evanisnor.flowmeter.system.NotificationSystem
+import com.evanisnor.flowmeter.system.WorkerFactoryFactory
+import com.squareup.anvil.annotations.MergeComponent
+import dagger.BindsInstance
+import dagger.Component
+
+/**
+ * Top-level component for [AppScope]
+ */
+@SingleIn(AppScope::class)
+@MergeComponent(AppScope::class)
+interface FlowmeterAppComponent {
+  fun activityInjectors(): Map<Class<out ComponentActivity>, AnvilInjector<*>>
+  fun workerFactoryFactory(): WorkerFactoryFactory
+  fun notificationSystem(): NotificationSystem
+
+  @Component.Builder
+  interface Builder {
+    @BindsInstance
+    fun context(context: Context): Builder
+    fun build(): FlowmeterAppComponent
+  }
+}
